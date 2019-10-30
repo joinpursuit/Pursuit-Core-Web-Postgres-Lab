75ffc07ac554d9ba2e8e0fe9d883adc0f65949d0
vonbarown <vonielbrown@pursuit.org>
2019-10-29 21:12:42 -0400

    starting to manipulate the database

--- assets/facebook_db.sql
+++ assets/facebook_db.sql
@@ -9,14 +9,15 @@ CREATE DATABASE facebook_db;
 CREATE TABLE users
 (
     id Serial PRIMARY KEY,
-    name VARCHAR,
+    fullname VARCHAR,
     age INT
 );
 
 CREATE TABLE posts
 (
     id Serial PRIMARY KEY,
-    poster_id INT REFERENCES users (id)
+    poster_id INT REFERENCES users(id),
+    body VARCHAR
 );
 
 CREATE TABLE likes
@@ -27,3 +28,7 @@ CREATE TABLE likes
 );
 
 -- Add and Manipulate Data
+INSERT INTO users (fullname, age) VALUES ('Victoria Adams', 47), ('Gerson Lopez', 33),('George Lopez',30),('Micheal Amparo',24),('Jenesh Napit',24);
+
+INSERT INTO posts (poster_id, body) VALUES (1,'Hello'),(1,'World'),(1,'Code'),(2,'Javascript'),(3,'ECMASCRIPT'),(4,'PYTHON'),(5,'C++');
+-- DELETE from posts
