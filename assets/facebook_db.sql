22c92db9e8433004dc7973be424b4f7d9049f47b
vonbarown <vonielbrown@pursuit.org>
2019-10-30 19:16:01 -0400

    updating users

--- assets/facebook_db.sql
+++ assets/facebook_db.sql
@@ -8,14 +8,14 @@ CREATE DATABASE facebook_db;
 
 CREATE TABLE users
 (
-    id Serial PRIMARY KEY,
+    id INT PRIMARY KEY,
     fullname VARCHAR,
     age INT
 );
 
 CREATE TABLE posts
 (
-    id Serial PRIMARY KEY,
+    id INT PRIMARY KEY,
     poster_id INT REFERENCES users(id),
     body VARCHAR
 );
@@ -28,7 +28,24 @@ CREATE TABLE likes
 );
 
 -- Add and Manipulate Data
-INSERT INTO users (fullname, age) VALUES ('Victoria Adams', 47), ('Gerson Lopez', 33),('George Lopez',30),('Micheal Amparo',24),('Jenesh Napit',24);
+-- INSERT INTO users (id,fullname, age) VALUES (20,'Victoria Adams', 47), (21,'Gerson Lopez', 33),(22,'George Lopez',30),(23,'Micheal Amparo',24),(24,'Jenesh Napit',24);
 
-INSERT INTO posts (poster_id, body) VALUES (1,'Hello'),(1,'World'),(1,'Code'),(2,'Javascript'),(3,'ECMASCRIPT'),(4,'PYTHON'),(5,'C++');
--- DELETE from posts
+-- INSERT INTO posts (poster_id, id, body) VALUES (20,3,'Hello'),(20,4,'World'),(20,5,'Code'),(21,6,'Javascript'),(22,7,'ECMASCRIPT'),(23,8,'PYTHON'),(24,9,'C++');
+-- INSERT INTO likes (id,liker_id,post_id) VALUES (11,20,3),(12,20,4),(13,20,5),(14,20,6),(15,20,7),(16,20,8),(17,20,9)
+-- INSERT INTO likes (id,liker_id,post_id) VALUES (18,21,6)
+-- INSERT INTO likes (id,liker_id,post_id) VALUES (18,22,6)
+
+
+UPDATE 
+    users
+SET 
+    fullname = 'Alfred'
+WHERE 
+    id = 22;
+
+UPDATE 
+    users
+SET 
+    age = 31
+WHERE 
+    id = 24;
