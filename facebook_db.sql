a21b309e6a050c79858bc40dced2c0e3a4469840
Suzette <suzetteislam@gmail.com>
2019-10-29 23:20:39 -0400

    Data manipulation

--- facebook_db.sql
+++ facebook_db.sql
@@ -12,7 +12,7 @@ CREATE TABLE users(
 
 CREATE TABLE posts(
     id SERIAL PRIMARY KEY, 
-    poster_id INT UNIQUE REFERENCES users(id),
+    poster_id INT REFERENCES users(id),
     body VARCHAR
 );
 
@@ -26,3 +26,17 @@ CREATE TABLE likes(
 
 INSERT INTO users(fullname, age)
 VALUES('Maliq Taylor', 23), ('Kadijah Wilson', 25), ('Michael Amparo', 24), ('Voniel Brown', 24), ('Giselle Sanchez', 24);
+
+INSERT INTO posts(poster_id, body)
+VALUES(1, 'Send help pls'), (1, 'Happy Tuesday!'), (1, 'I like space'), (2, 'Hope this works'), (3, 'hi'), (4, 'Apparently one of the Roman emperors marches his army to the sea and ordered them to thow their spears into the sea because he wanted to fight Poseidon'), (5, '*insert dog meme here*');
+
+INSERT INTO likes(liker_id, post_id)
+VALUES (1, 4), (1, 5), (1,6), (1,7), (2, 4), (3, 1), (3, 2), (3, 3), (3, 4), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5);
+
+UPDATE users SET age = 31 WHERE id = 5;
+
+UPDATE users SET fullname = 'Alfred' WHERE id = 3;
+
+DELETE FROM likes WHERE liker_id = 1 AND post_id= 4;
+
+DELETE FROM posts WHERE poster_id= 1 AND 
