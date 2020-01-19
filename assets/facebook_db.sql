ddd4766535b131b7a747a8b1a786f9110fe52e9c
Wilsantos1975 <faustosantos@pursuit.org>
2020-01-19 14:25:10 -0500

    tables created

--- assets/facebook_db.sql
+++ assets/facebook_db.sql
@@ -3,7 +3,29 @@ CREATE DATABASE facebook_db;
 
 \c facebook_db;
 
+DROP TABLE IF EXISTS users;
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    poster_id INT REFERENCES users (id),
+    body TEXT
+);
+
+DROP TABLE IF EXISTS likes;
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    liker_id INT REFERENCES users (id),
+    post_id INT REFERENCES posts (id)
+);
 
 
 
