8807af7e7ea5d2a97a722908ea0e2fb872a260be
Wilsantos1975 <faustosantos@pursuit.org>
2020-01-19 14:56:55 -0500

    seed data into tables

--- assets/facebook_db.sql
+++ assets/facebook_db.sql
@@ -1,3 +1,5 @@
+-- Database & Table Creation
+
 DROP DATABASE IF EXISTS facebook_db;
 CREATE DATABASE facebook_db;
 
@@ -26,6 +28,48 @@ CREATE TABLE likes (
     liker_id INT REFERENCES users (id),
     post_id INT REFERENCES posts (id)
 );
+
+-- Add and Manipulate Data
+INSERT INTO users
+VALUES ('Wil', 36),
+    ('Karen', 21),
+    ('Corey', 100),
+    ('Jon', 26),
+    ('Jhenya', 21);
+
+INSERT INTO posts
+VALUES (1,'AWESOME!'),
+        (4,'Ok, Ok'),
+        (1,'Love it!'),
+        (3,'Clap your hands if you can hear my voice'),
+        (2, 'SQL ROCKS JS SUCKS!!'),
+        (1,'Cool Beans'),
+        (5,'Bonjour');
+
+INSERT INTO likes
+VALUES (1,4),
+    (1,5),
+    (1,7),
+    (2,4),
+    (3,1),
+    (3,2),
+    (3,3),
+    (1,6),
+    (4,4),
+    (4,5),
+    (5,3),
+    (3,4),
+    (4,7),
+    (5,1),
+    (5,2),
+    (5,4),
+    (5,6);
+
+
+
+
+
+
 
 
 
