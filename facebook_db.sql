cc3fa487c158cdb949ea5edea30f140ce548f4a5
maria liza <mariamartinez@pursuit.org>
2020-01-19 15:18:31 -0500

    populate tables

--- facebook_db.sql
+++ facebook_db.sql
@@ -1,28 +1,32 @@
 DROP DATABASE IF EXISTS facebook_db;
 CREATE DATABASE facebook_db;
 
-\connect facebook_db;
+\c facebook_db;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
 
-CREATE TABLE users(
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    age INTEGER,
+    age INTEGER
 );
 
 CREATE TABLE posts(
     id SERIAL PRIMARY KEY,
     poster_id INT REFERENCES users(id),
-    body TEXT,
+    body TEXT
 
 );
-CREATE TABLE likes(
+CREATE TABLE likes (
     id SERIAL PRIMARY KEY,
     liker_id INT REFERENCES users(id),
-    post_id INT REFERENCES post(id)
+    post_id INT REFERENCES posts(id)
 );
 INSERT INTO users (name, age)
 VALUES ('deja', 35), ('jon', 26), ('maria', 21), ('alex', 16), ('socrates',3);
-INSERT INTO posts (name, body)
-VALUES ('deja', 'hi'), ('jon','bye'),('jon', 'hi'), ('jon','bye'),('maria', 'yerrrr'), ('alex', 'meow'),('socrates', 'bloop')
 
+INSERT INTO posts (poster_id, body)
+VALUES (1, 'hi'), (2,'bye'),(3, 'hi'), (4,'bye'),(5, 'yerrrr'), (5, 'meow'),(5, 'bloop');
+
+INSERT INTO likes (liker_id, post_id)
+VALUES (1, 5), (2, 1), (2, 3), (2, 5), (3, 1), (3, 5), (4, 1), (5, 1), (2, 2), (2, 3);
