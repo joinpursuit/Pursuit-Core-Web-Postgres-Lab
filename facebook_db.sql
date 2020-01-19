c4d3b956a03c323d59fd32e7ddd3fc2a3f8d8a56
crystal cardona <crystalcardona@pursuit.org>
2020-01-19 14:34:37 -0500

    added insert into users

--- facebook_db.sql
+++ facebook_db.sql
@@ -2,3 +2,28 @@ DROP DATABASE IF EXISTS facebook_db
 CREATE DATABASE facebook_db
 
 \c facebook_db
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES users(id) ON DELETE SET NULL,
+    body TEXT
+);
+
+CREATE TABLE  likes (
+    id SERIAL PRIMARY KEY,
+    liker_id INT REFERENCES users(id) ON DELETE SET NULL,
+    post_id INT REFERENCES post(id) ON DELETE SET NULL,
+);
+
+INSERT INTO users 
+    VALUES (1, 'Jon', 69),
+        (2, 'Corey', 100),
+        (3, 'Kong', 25),
+        (4, 'Henry', 101),
+        (5, 'David', 5);
